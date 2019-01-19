8d1073b2be72072b4d33882c25db45c7b983b802
jandraszyk <jasiek2695@gmail.com>
2019-01-19 11:17:39 +0100

    Gather stats

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,11 @@ import javax.swing.*;
 import java.awt.Point;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
-    private static final String FILENAME = "kroa100.tsp";
+    private static final String FILENAME = "kroa150.tsp";
     private static final long BRUTE_FORCE_EPOCH_NUMBER = 1_000_000;
 
     public static void main(String[] args) {
@@ -29,21 +27,23 @@ public class Main {
         LocalSearch localSearchRP = new LocalSearch(randomPath);
         LocalSearch localSearchBF = new LocalSearch(bruteForceSearch);
         LocalSearch localSearchNN = new LocalSearch(nearestNeighbor);
-        IteratedLocalSearch iteratedLocalSearchNN = new IteratedLocalSearch(localSearchNN);
 
-        runAlgorithm(bruteForceSearch);
-        runAlgorithm(randomPath);
-        runAlgorithm(nearestNeighbor);
-        runAlgorithm(localSearchRP);
-        runAlgorithm(localSearchBF);
-        runAlgorithm(localSearchNN);
+//        runAlgorithm(bruteForceSearch);
+//        runAlgorithm(randomPath);
+//        runAlgorithm(nearestNeighbor);
+//        runLocalSearchNN(nearestNeighbor, localSearchNN, list);
+//        runLocalSearchBrute(bruteForceSearch, localSearchBF, list);
+//        runAlgorithm(localSearchRP);
+//        runAlgorithm(localSearchBF);
+//        runAlgorithm(localSearchNN);
+//        SimilarEdges similarEdges = new SimilarEdges(list);
+//        similarEdges.execute();
+//        Map<Double, Double> allCostsEdges = similarEdges.getAllCostsEdges();
+//        for(Double key : allCostsEdges.keySet()) {
+//            System.out.println(key + "," + allCostsEdges.get(key));
+//        }
+        IteratedLocalSearch iteratedLocalSearchNN = new IteratedLocalSearch(localSearchNN);
         runAlgorithm(iteratedLocalSearchNN);
-        SimilarEdges similarEdges = new SimilarEdges(list);
-        similarEdges.execute();
-        Map<Double, Double> allCostsEdges = similarEdges.getAllCostsEdges();
-        for(Double key : allCostsEdges.keySet()) {
-            System.out.println(key + "," + allCostsEdges.get(key));
-        }
 //        drawSimilarEdgesVsCosts(allCostsEdges);
 //        double bestLengthNN = nearestNeighbor.getMinDistance();
 //        LocalSearch bestNN = null;
@@ -68,6 +68,31 @@ public class Main {
 //        System.out.println("Execution time: " + Duration.between(t1, t2));
     }
 
+    private static long runMSLS(NearestNeighbor nearestNeighbor, LocalSearch localSearchNN, List<Point> list) {
+        double bestLengthNN = nearestNeighbor.getMinDistance();
+        LocalSearch bestNN = null;
+        Instant t1 = Instant.now();
+        for (int i = 0; i < 100; i++) {
+            nearestNeighbor = new NearestNeighbor(list, "Nearest Neighbor");
+            localSearchNN = new LocalSearch(nearestNeighbor);
+            Paths pathsNN = localSearchNN.resolvePath();
+            double pathLengthNN = PathLength.getTotalPathLength(pathsNN);
+            if(pathLengthNN < bestLengthNN) {
+                bestLengthNN = pathLengthNN;
+                bestNN = localSearchNN;
+            }
+        }
+        Instant t2 = Instant.now();
+        System.out.println("=========================================================");
+        String title = "100 uruchomień NN";
+        System.out.println(title);
+        bestNN.printStatistics();
+        PointsVisualizer pointsVisualizer = new PointsVisualizer(bestNN.getBasePath());
+        pointsVisualizer.draw(title);
+        System.out.println("Execution time: " + Duration.between(t1, t2));
+        return Duration.between(t1,t2).getSeconds();
+    }
+
     private static void runAlgorithm(PathFinder pathFinder) {
         System.out.println("=========================================================");
         String title = pathFinder.getName();
@@ -84,10 +109,84 @@ public class Main {
         PointsVisualizer visualizer = new PointsVisualizer(optimalPath);
         visualizer.draw(title);
     }
+    private static void runLocalSearchNN(NearestNeighbor nearestNeighbor, LocalSearch localSearchNN, List<Point> list) {
+        double bestLengthNN = nearestNeighbor.getMinDistance();
+        double maxLengthNN = 0.0;
+        List<Integer> allTimes = new ArrayList<>();
+        List<Double> allDistance = new ArrayList<>();
+        LocalSearch bestNN = null;
+        for (int i = 0; i < 10; i++) {
+            nearestNeighbor = new NearestNeighbor(list, "Nearest Neighbor");
+            localSearchNN = new LocalSearch(nearestNeighbor);
+            Instant t1 = Instant.now();
+            Paths pathsNN = localSearchNN.resolvePath();
+            Instant t2 = Instant.now();
+            allTimes.add(Duration.between(t1,t2).getNano()/1000000);
+            double pathLengthNN = PathLength.getTotalPathLength(pathsNN);
+            allDistance.add(pathLengthNN);
+            if(pathLengthNN < bestLengthNN) {
+                bestLengthNN = pathLengthNN;
+                bestNN = localSearchNN;
+            } else if (pathLengthNN > maxLengthNN) {
+                maxLengthNN = pathLengthNN;
+            }
+        }
+        Integer minTime = allTimes.stream().mapToInt(v -> v).min().orElseThrow(NoSuchElementException::new);
+        Integer maxTime = allTimes.stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new);
+        Double avgDistance = setAvgDistance(allDistance);
+        System.out.println("=========================================================");
+        String title = "Local Search (Nearest Neighbor)";
+        System.out.println(title);
+        PathFinder.stat(bestNN.getBasePath(), bestLengthNN,maxLengthNN,avgDistance);
+        System.out.println("Max Time(ms): " + maxTime);
+        System.out.println("Min Time(ms): " + minTime);
+        PointsVisualizer pointsVisualizer = new PointsVisualizer(bestNN.getBasePath());
+        pointsVisualizer.draw(title);
+    }
 
-    private static void drawSimilarEdgesVsCosts(Map<Double,Double> allCostsEdges){
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900,600);
+    private static void runLocalSearchBrute(BruteForceSearch bruteForceSearch, LocalSearch localSearchBF, List<Point> list) {
+        double bestLengthBF = bruteForceSearch.getMinDistance();
+        double maxLengthBF = 0.0;
+        List<Integer> allTimes = new ArrayList<>();
+        List<Double> allDistance = new ArrayList<>();
+        LocalSearch bestBF = null;
+        for (int i = 0; i < 10; i++) {
+            bruteForceSearch = new BruteForceSearch(list, "Brute force", BRUTE_FORCE_EPOCH_NUMBER);
+            localSearchBF = new LocalSearch(bruteForceSearch);
+            Instant t1 = Instant.now();
+            Paths pathsBF = localSearchBF.resolvePath();
+            Instant t2 = Instant.now();
+            allTimes.add(Duration.between(t1,t2).getNano()/1000000);
+            double pathLengthBF = PathLength.getTotalPathLength(pathsBF);
+            allDistance.add(pathLengthBF);
+            if(pathLengthBF < bestLengthBF) {
+                bestLengthBF = pathLengthBF;
+                bestBF = localSearchBF;
+            } else if (pathLengthBF > maxLengthBF) {
+                maxLengthBF = pathLengthBF;
+            }
+        }
+        Integer minTime = allTimes.stream().mapToInt(v -> v).min().orElseThrow(NoSuchElementException::new);
+        Integer maxTime = allTimes.stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new);
+        Double avgDistance = setAvgDistance(allDistance);
+        System.out.println("=========================================================");
+        String title = "Local Search (Random)";
+        System.out.println(title);
+        PathFinder.stat(bestBF.getBasePath(), bestLengthBF,maxLengthBF,avgDistance);
+        System.out.println("Max Time(ms): " + maxTime);
+        System.out.println("Min Time(ms): " + minTime);
+        PointsVisualizer pointsVisualizer = new PointsVisualizer(bestBF.getBasePath());
+        pointsVisualizer.draw(title);
+    }
+
+    private static double setAvgDistance(List<Double> allDistance) {
+        double sum = 0;
+        if(!allDistance.isEmpty()) {
+            for(Double distance : allDistance) {
+                sum+= distance;
+            }
+            return sum / allDistance.size();
+        }
+        return sum;
     }
 }
